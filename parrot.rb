6d5cb00abf83246c56a725f7e669902342781928
Dametrik Fick <dametrik.fick@gmail.com>
2020-03-04 01:56:51 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,23 +1,4 @@
-def parrot
-  puts "Squawk!"
-end
-
-parrot
-
-def parrot
-  return "Squawk!"
-end
-
-parrot
-
-def parrot(phrase = "Pretty bird!")
-  puts "#{phrase}"
-end
-
-parrot("Pretty bird!")
-
 def parrot(phrase = "Squawk!")
+  puts "#{phrase}"
   return "#{phrase}"
-end
-
-parrot("Squawk!")
+  return "Pretty bird!"
